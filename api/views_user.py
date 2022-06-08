ceeabc39d96ad3c56bcb6cd42b4ca3b5010584be
Bryan Bordeman <bryanbordeman@Bryans-MacBook-Pro.local>
2022-06-08 06:54:39 -0400

    Omitted users object from login

--- api/views_user.py
+++ api/views_user.py
@@ -53,38 +53,14 @@ def login(request):
                 try:
                     token = Token.objects.get(user=user)
                     user_information = User.objects.get(id=user.id)
-                    all_users_object = User.objects.all()
-
-                    users = {}
-
-                    for item in all_users_object:
-                        users[f'{item.first_name} {item.last_name}'] = {'id': item.id, 
-                        'username': item.username,
-                        'email': item.email,
-                        'first_name': item.first_name,
-                        'last_name': item.last_name,
-                        'is_staff': item.is_staff}
-                    
-                    users_json_object = json.dumps(users, indent = 4)
                     
                 except: #if token not in db, create a new one
                     token = Token.objects.create(user=user)
-            if user.is_staff:
-                print(all_users_object)
-                return JsonResponse({'token' : str(token), 
-                                    'user' : str(user_information.username),
-                                    'user_email' : str(user_information.email),
-                                    'user_first_name' : str(user_information.first_name),
-                                    'user_last_name' : str(user_information.last_name),
-                                    'user_is_staff' : str(user_information.is_staff),
-                                    'users' : users_json_object
-                                    }, status=201)
-            else:
-                print('not staff')
-                return JsonResponse({'token' : str(token), 
-                                    'user' : str(user_information.username),
-                                    'user_email' : str(user_information.email),
-                                    'user_first_name' : str(user_information.first_name),
-                                    'user_last_name' : str(user_information.last_name),
-                                    'user_is_staff' : str(user_information.is_staff),
-                                    }, status=201)
+
+            return JsonResponse({'token' : str(token), 
+                                'user' : str(user_information.username),
+                                'user_email' : str(user_information.email),
+                                'user_first_name' : str(user_information.first_name),
+                                'user_last_name' : str(user_information.last_name),
+                                'user_is_staff' : str(user_information.is_staff),
+                                }, status=201)
