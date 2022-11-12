7b87a0b24d2b75e02434d8ab9e94c641d3adca0e
Bryan Bordeman <bryanbordeman@Bryans-MBP.home>
2022-11-12 07:38:26 -0500

    Updated Project model

--- project/admin.py
+++ project/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
-from .models import ProjectCategory, ProjectType, Project
+from .models import ProjectCategory, ProjectType, BillingType, OrderType, Project
 
 admin.site.register(ProjectCategory)
 admin.site.register(ProjectType)
+admin.site.register(BillingType)
+admin.site.register(OrderType)
 admin.site.register(Project)
--- project/models.py
+++ project/models.py
@@ -16,6 +16,19 @@ class ProjectType(models.Model):
     def __str__(self):
         return self.name
 
+class BillingType(models.Model):
+    name = models.CharField(max_length=200)
+    code = models.CharField(max_length=3)
+
+    def __str__(self):
+        return self.name
+class OrderType(models.Model):
+    name = models.CharField(max_length=200)
+    code = models.CharField(max_length=3)
+
+    def __str__(self):
+        return self.name
+
 class Project(models.Model):
     is_active = models.BooleanField(null= False, default=True)
     number = models.CharField(max_length=8, null= True)
@@ -23,9 +36,17 @@ class Project(models.Model):
     project_category = models.ForeignKey(ProjectCategory, on_delete=models.SET_NULL, null=True)
     project_type = models.ForeignKey(ProjectType, on_delete=models.SET_NULL, null=True)
     address = models.ForeignKey('contact.Address', null=True, blank=True, on_delete=models.PROTECT)
-    # customer_company = models.ForeignKey('contact.Company', null=True, blank=True, on_delete=models.PROTECT)
+    terms = models.CharField(max_length=200, null= True)
     prevailing_rate = models.BooleanField(null= False, default=False)
+    union = models.BooleanField(null= False, default=False)
+    certified_payroll = models.BooleanField(null= False, default=False)
+    material_only = models.BooleanField(null= False, default=False)
     travel_job = models.BooleanField(null= False, default=False)
+    tax_exempt = models.BooleanField(null= False, default=False)
+    billing_type = models.ForeignKey(BillingType, on_delete=models.SET_NULL, null=True)
+    order_type = models.ForeignKey(OrderType, on_delete=models.SET_NULL, null=True)
+    price = models.FloatField(null=True, blank=True, default=0)
+
     notes = models.TextField(max_length=250, blank=True,
                                 validators=[MaxLengthValidator(250)])
 
