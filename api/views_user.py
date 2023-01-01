b33bf152cf073b968e1544e5734ac564e0aded85
Bryan Bordeman <bryanbordeman@Bryans-MBP.home>
2023-01-01 17:11:35 -0500

    added order by first name in user view

--- api/views_user.py
+++ api/views_user.py
@@ -18,7 +18,7 @@ class UserView(generics.ListAPIView):
     permissions_classes = [permissions.IsAuthenticated]
 
     def get_queryset(self):
-        return User.objects.filter(is_active=True).all()
+        return User.objects.filter(is_active=True).all().order_by('first_name')
 
 @csrf_exempt
 def signup(request):
