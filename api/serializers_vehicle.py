431d7ace7840e9bd3dbc7f772567f9f331be629a
Bryan Bordeman <bryanbordeman@Bryans-MacBook-Pro.local>
2023-02-01 05:56:43 -0500

    Added depth to serializers_vehicle

--- api/serializers_vehicle.py
+++ api/serializers_vehicle.py
@@ -1,8 +1,11 @@
 from rest_framework import serializers
 from schedule.models import Vehicle
+from api.serializers_user import UserSerializer
 
 class  VehicleSerializer(serializers.ModelSerializer):
     '''Vehicle serializer'''
+    assignment = UserSerializer()
     class Meta:
         model = Vehicle
         fields = '__all__'
+        depth = 1
