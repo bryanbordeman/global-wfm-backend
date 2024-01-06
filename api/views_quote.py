805664bf554fdd2e6a706b1eadb39055f5d227c6
Bryan Bordeman <bryanbordeman@MacBook-Pro.home>
2024-01-06 06:19:21 -0500

    Updated QuoteData in views_quote

--- api/views_quote.py
+++ api/views_quote.py
@@ -2,8 +2,8 @@ from rest_framework import generics, permissions
 from .serializers_quote import QuoteSerializer, QuoteCreateSerializer, QuoteToggleSerializer
 from quote.models import Quote as QuoteModel
 from project.models import Project as ProjectModel
-from project.models import ProjectCategory as ProjectCategoryModel
-from project.models import ProjectType as ProjectTypeModel
+from project.models import HSE as HSEModel
+from project.models import Service as ServiceModel
 from django.http import JsonResponse
 from django.views.decorators.csrf import csrf_exempt
 from django.forms.models import model_to_dict
@@ -44,18 +44,17 @@ class QuoteToggleArchive(generics.UpdateAPIView):
 
     def get_queryset(self):
         return QuoteModel.objects.all()
-    
+
     def perform_update(self, serializer):
         serializer.instance.is_active=not(serializer.instance.is_active)
         serializer.save()
-
 class QuoteCreate(generics.ListCreateAPIView):
     serializer_class = QuoteCreateSerializer
     permissions_classes = [permissions.IsAuthenticated]
 
     def get_queryset(self):
         return QuoteModel.objects.all()
-    
+
     def perform_create(self, serializer):
         # number = self.request.POST['number']
         # if QuoteModel.objects.filter(number=number).exists():
@@ -76,10 +75,10 @@ def NextQuoteNumber(request):
     '''Get the Next Quote Number'''
     year = time.strftime("%Y")[2:]
     if request.method == 'GET':
-        #! what if its not sequential and we manually enter old quote?? 
+        #! what if its not sequential and we manually enter old quote??
         try:
-            last_quote = model_to_dict(QuoteModel.objects.all().order_by('-number').first())  # omitted filter active. Might be an issue when DB gets large?
-            
+            last_quote = model_to_dict(QuoteModel.objects.all().order_by('-number').first()) # omitted filter active. Might be an issue when DB gets large?
+
             last_quote_number = (last_quote['number'])
             current_quote_year = last_quote_number[1:3]
 
@@ -92,8 +91,8 @@ def NextQuoteNumber(request):
             else:
                 next_number = '001'
                 next_number_str = f'Q{year}-{str(next_number)}'
-        
-        except AttributeError: 
+
+        except AttributeError:
             #if database is empty
             last_quote_number = None  #Doesn't exist, set to None
             next_number_str = f'Q{year}-001'
@@ -105,7 +104,7 @@ def LastQuote(request):
     '''Get the Last Quote Number'''
     if request.method == 'GET':
         last_quote = model_to_dict(QuoteModel.objects.filter(is_active=True).order_by('-number').first())
-        
+
         last_quote_id = (last_quote['id'])
 
         return JsonResponse({'last_quote_id': str(last_quote_id)}, status=201)
@@ -115,30 +114,48 @@ def QuoteData(request, year):
     '''Get totals for project_category and project_type'''
 
     if request.method == 'GET':
-        data = {'quote_count': 0, 'project_count': 0,'outstanding_sales_sum' : 0, 'sold_sales_sum' : 0}
-        outstanding_sales_sum = QuoteModel.objects.filter(is_active=True, number__startswith=f'Q{str(year)[-2:]}').aggregate(Sum('price'))
-        sold_sales_sum = ProjectModel.objects.filter(number__endswith=str(year)[-2:]).aggregate(Sum('price'))
-        # sum = QuoteModel.objects.aggregate(Sum('project_category'))
+        data = {'quote_count': 0,
+                'project_count': 0,
+                'hse_count': 0,
+                'service_count': 0,
+                'outstanding_sales_sum': 0,
+                'sold_sales_sum': 0,
+                'projects_sold_sales_sum': 0,
+                'hses_sold_sales_sum': 0,
+                'services_sold_sales_sum': 0
+                }
 
+        # Calculate outstanding sales sum
+        outstanding_sales_sum = QuoteModel.objects.filter(is_active=True, number__startswith=f'Q{str(year)[-2:]}').aggregate(Sum('price'))
+        data['outstanding_sales_sum'] = round(outstanding_sales_sum['price__sum'] or 0, 2)
+
+        hse_substring_to_check = "HSE" + str(year)[-2:]
+        service_substring_to_check = "SVC" + str(year)[-2:]
+
+        # Calculate sold sales sum
+        projects_sold_sales_sum = ProjectModel.objects.filter(number__endswith=str(year)[-2:]).aggregate(Sum('price'))
+        hses_sold_sales_sum = HSEModel.objects.filter(number__startswith=hse_substring_to_check).aggregate(Sum('price'))
+        services_sold_sales_sum = ServiceModel.objects.filter(number__startswith=service_substring_to_check).aggregate(Sum('price'))
+
+        sold_sales_sum = (float(projects_sold_sales_sum['price__sum'] or 0) +
+                        float(hses_sold_sales_sum['price__sum'] or 0) +
+                        float(services_sold_sales_sum['price__sum'] or 0))
+
+        data['sold_sales_sum'] = round(sold_sales_sum, 2)
+        data['projects_sold_sales_sum'] = projects_sold_sales_sum
+        data['hses_sold_sales_sum'] = hses_sold_sales_sum
+        data['services_sold_sales_sum'] = services_sold_sales_sum
+
+
+        # Count quotes and projects
         quotes = QuoteModel.objects.filter(number__startswith=f'Q{str(year)[-2:]}').order_by('-number')
         projects = ProjectModel.objects.filter(number__endswith=str(year)[-2:]).order_by('-number')
-        category_object = ProjectCategoryModel.objects.all().values()
-        type_object = ProjectTypeModel.objects.all().values()
+        HSE = HSEModel.objects.filter(number__startswith=hse_substring_to_check).order_by('-number')
+        service = ServiceModel.objects.filter(number__startswith=service_substring_to_check).order_by('-number')
 
+        data['quote_count'] = quotes.count()
+        data['project_count'] = projects.count() + HSE.count() + service.count()
+        data['hse_count'] = HSE.count()
+        data['service_count'] = service.count()
 
-        data['quote_count'] = str(quotes.count())
-        data['project_count'] = str(projects.count())
-        data['outstanding_sales_sum'] = str(round(outstanding_sales_sum['price__sum'],2))
-        data['sold_sales_sum'] = str(round(sold_sales_sum['price__sum'],2))
-        # print(category_object)
-        # print(type_object)
-
-        # category_object = ProjectCategoryModel.objects.all().values()
-
-        # for i in category_object:
-        #     print(i['id'], i['name'])
-
-        # last_quote = model_to_dict(QuoteModel.objects.filter(is_active=True).order_by('-number').first())
-        # last_quote_id = (last_quote['id'])
-        return JsonResponse(data, status=201)
-
+        return JsonResponse(data, status=200)
